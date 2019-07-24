db92fdb544cc5b09c8b192349f7c3121fe3bd4f0
Muhammad Shiblee <m.shiblee@outlook.com>
2019-07-24 18:47:27 -0400

    Internal documentation updated.

--- BMICalculator/Program.cs
+++ BMICalculator/Program.cs
@@ -3,7 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+/*Assignment 4, BMI Calculator
+ * MUHAMMAD SHIBLEE
+ * 301064587
+ * Last modified on 24 July, 2019
+ */
 namespace BMICalculator
 {
     static class Program
